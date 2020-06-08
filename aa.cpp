f0ae3fcd7bf96fe084c2e2c37905e58cd23b6ea2
layse-garcia <63252079+layse-garcia@users.noreply.github.com>
2020-06-08 20:32:15 -0300

    lagrimax2

--- aa.cpp
+++ aa.cpp
@@ -39,7 +39,7 @@ int main(){
 	cin >> qtdCasos; //a
 	
 	for (int i = 0; i < qtdCasos; i++){ //a
-		cout << "Caso " << i + 1 << endl;
+		
 		// Entrada da quantidadde de vértices e arestas a serem inseridas
 		cin >> totalVertices >> totalArestas; //b>>c
 		
@@ -60,12 +60,11 @@ int main(){
 			vis2[vDe] = true; //d
 			vis2[vPara] = true; //e
 		}
+		cout << "Caso " << i + 1 << endl;
 		
 		for (int j = 0; j < totalVertices; j++){ //b
 			// Pega qualquer valor entre início e o fim do grafo
-			sort(meuGrafo[j].begin(), meuGrafo[j].end()); //g
-		}
-		for (int j = 0; j < totalVertices; j++){ //b
+			sort(meuGrafo[j].begin(), meuGrafo[j].end());
 			// Se o vértice não foi visitado
 			if (!vis[j]){
 				// Faz a DFS no indice do loop
@@ -78,3 +77,62 @@ int main(){
 		}
 	}
 }
+
+/*
+void iniciaGrafo(int vertices, vector<int> grafo[]){
+	for (int j = 0; j < vertices; j++){//b
+		// Limpa o grafo, pode-se dizer que limpa os valores lixos, caso houver
+		grafo[j].clear();
+			
+		// ################# explicar isso aqui melhor
+		memset(vis, 0, sizeof vis);
+		memset(vis2, 0, sizeof vis2);
+	}
+}
+
+int main(){
+	/* -- > VARIÁVEIS UTILIZADAS:
+		* qtdCasos: quantidade de Casos de teste
+		* totalVertices: quantidade de vertices que serão criados para aquele determinado caso
+		* totalArestas: quantidade de arestas que serão inseridas para aquele determinado caso
+		* vDe: De um vértice (ligando como se fosse uma aresta)
+		* vPara: Para outro vértice (ligando como se fosse uma aresta)
+	 
+	int qtdCasos, totalVertices, totalArestas, vDe, vPara; // a,b,c,d,e
+	
+	// Entrada da quantidade de Casos
+	cin >> qtdCasos; //a
+	
+	for (int i = 0; i < qtdCasos; i++){ //a
+		
+		// Entrada da quantidadde de vértices e arestas a serem inseridas
+		cin >> totalVertices >> totalArestas; //b>>c
+		
+		iniciaGrafo(totalVertices, &meuGrafo[i]);
+		
+		// Indicação das arestas
+		for (int j = 0; j < totalArestas; j++){ //c
+			cin >> vDe >> vPara; // d>>e
+			// Coloca o elemento na última posição do vetor ## MELHORAR
+			meuGrafo[vDe].push_back(vPara);//[d] (e)
+			vis2[vDe] = true; //d
+			vis2[vPara] = true; //e
+		}
+		cout << "Caso " << i + 1 << endl;
+		
+		for (int j = 0; j < totalVertices; j++){ //b
+			// Pega qualquer valor entre início e o fim do grafo
+			sort(meuGrafo[j].begin(), meuGrafo[j].end());
+			// Se o vértice não foi visitado
+			if (!vis[j]){
+				// Faz a DFS no indice do loop
+				dfs(j, 1); 
+				// Se j já foi visitado, então salto uma linha
+				if (vis2[j]){ 
+					cout << endl;
+				}
+			}
+		}
+	}
+}
+*/

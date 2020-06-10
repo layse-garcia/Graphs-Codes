43fbd10f30a2b93c782aa007f796294846224589
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-06-09 21:43:53 -0300

    Alterando identação e comentários

--- ex2.cpp
+++ ex2.cpp
@@ -9,71 +9,68 @@ using namespace std;
 typedef pair<unsigned, unsigned> meuPar; 
 
 // Função responsável por adicionar uma aresta valorada
-void adicionaAresta(vector<pair<unsigned, unsigned>> grafo[], unsigned de, unsigned para, unsigned valor) 
-{ 
-    grafo[de].push_back(make_pair(para, valor)); 
-    grafo[para].push_back(make_pair(de, valor)); 
-} 
+void adicionaAresta(vector<pair<unsigned, unsigned>> grafo[], unsigned de, unsigned para, unsigned valor) {
+	
+	grafo[de].push_back(make_pair(para, valor));
+	grafo[para].push_back(make_pair(de, valor));
+	
+}
 
 void primAGM(vector<pair<unsigned,unsigned> > grafo[], unsigned vertice){
-	// Create a priority queue to store vertices that 
-    // are being preinMST. This is weird syntax in C++. 
-    // Refer below link for details of this syntax 
-    // http://geeksquiz.com/implement-min-heap-using-stl/ 
-	priority_queue<meuPar, vector<meuPar>, greater<meuPar>> filaPrioridade; 
+	
+	// Fila de prioridade para armazenar os vertices para a AGM
+	filaPrioridade<meuPar, vector<meuPar>, greater<meuPar>> filaPrioridade; 
+
+	int inicio = 0;
+
+	// Criando vetor para as chaves e inicializa como infinito
+	vector<unsigned> valor(vertice, INF); 
+
+	// Array para armazenar os pais
+	vector<int> pai(vertice, -1); 
+
+	// Vetor para armaenar os vertices já inseridos na AGM
+	vector<bool> inserido(vertice, false); 
+
+	// Inserindo a raiz na fila de prioridade
+	filaPrioridade.push(make_pair(0, inicio)); 
+	valor[inicio] = 0; 
+
+	/* Looping till priority queue becomes empty */
+	while (!filaPrioridade.empty()) 
+	{ 
+		// The first vertex in pair is the minimum key 
+		// vertex, extract it from priority queue. 
+		// vertex label is stored in second of pair (it 
+		// has to be done this way to keep the vertices 
+		// sorted key (key must be first item 
+		// in pair) 
+		unsigned u = filaPrioridade.top().second; 
+		filaPrioridade.pop(); 
+
+		inserido[u] = true; // Include vertex in MST 
+
+		// Traverse all adjacent of u 
+		for (auto x : grafo[u]) 
+		{ 
+			// Get vertex label and weight of current adjacent 
+			// of u. 
+			unsigned v = x.first; 
+			unsigned valorU = x.second; 
   
-    int inicio = 0; // Taking vertex 0 as source 
-  
-    // Create a vector for keys and initialize all 
-    // keys as infinite (INF) 
-    vector<unsigned> valor(vertice, INF); 
-  
-    // To store parent array which in turn store MST 
-    vector<int> pai(vertice, -1); 
-  
-    // To keep track of vertices included in MST 
-    vector<bool> inserido(vertice, false); 
-  
-    // Insert source itself in priority queue and initialize 
-    // its key as 0. 
-    filaPrioridade.push(make_pair(0, inicio)); 
-    valor[inicio] = 0; 
-  
-    /* Looping till priority queue becomes empty */
-    while (!filaPrioridade.empty()) 
-    { 
-        // The first vertex in pair is the minimum key 
-        // vertex, extract it from priority queue. 
-        // vertex label is stored in second of pair (it 
-        // has to be done this way to keep the vertices 
-        // sorted key (key must be first item 
-        // in pair) 
-        unsigned u = filaPrioridade.top().second; 
-        filaPrioridade.pop(); 
-  
-        inserido[u] = true; // Include vertex in MST 
-  
-        // Traverse all adjacent of u 
-        for (auto x : grafo[u]) 
-        { 
-            // Get vertex label and weight of current adjacent 
-            // of u. 
-            unsigned v = x.first; 
-            unsigned valorU = x.second; 
-  
-            // If v is not in MST and weight of (u,v) is smaller 
-            // than current key of v 
-            if (inserido[v] == false && valor[v] > valorU) 
-            { 
-                // Updating key of v
-                valor[v] = valorU; 
-                filaPrioridade.push(make_pair(valor[v], v)); 
-                pai[v] = u;
-            } 
-        } 
-    } 
+			// If v is not in MST and weight of (u,v) is smaller 
+			// than current key of v 
+			if (inserido[v] == false && valor[v] > valorU) 
+			{ 
+				// Updating key of v
+				valor[v] = valorU; 
+				filaPrioridade.push(make_pair(valor[v], v)); 
+				pai[v] = u;
+			} 
+		} 
+	} 
 	unsigned soma = 0;
-    // Print edges of MST using parent array 
+	// Print edges of MST using parent array 
 	for (unsigned i = 1; i < vertice; i++){
 		printf("%d - %d\n", pai[i], i); 
 		soma += valor[i];
@@ -83,16 +80,16 @@ void primAGM(vector<pair<unsigned,unsigned> > grafo[], unsigned vertice){
 
 
 int main(){
+	
 	unsigned totalVertices, totalArestas, deVertice, paraVertice, custoAresta;
-
 	
 	while(true){
+		
 		cin >> totalVertices >> totalArestas;
+		
 		vector<meuPar> meuGrafo[totalVertices];
-		// Criação do grafo(Matriz de Adjacencias
 		
-		// Limpa a estrutra, pode-se dizer que limpa os valores lixos, caso houver
-		
+		// Criação do grafo(Matriz de Adjacencias)
 		
 		for(unsigned i = 0; i < totalArestas; i++){
 			cin >> deVertice >> paraVertice >> custoAresta;
@@ -102,9 +99,6 @@ int main(){
 		}
 		
 		primAGM(meuGrafo, totalVertices);
-		
-		
-		
 	}
 	
 }

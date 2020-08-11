11b38a7f5a557245c02fe5223b9386dcaf0310fa
layse-garcia <63252079+layse-garcia@users.noreply.github.com>
2020-08-11 20:59:47 -0300

    Realizar testes

--- exercícios/REO5/tsp.cpp
+++ exercícios/REO5/tsp.cpp
@@ -25,18 +25,29 @@ using namespace std;
 // Classe Grafo
 class Grafo{
 	private:
-		int vertices; // numero de vértices
-		int pontosX[MAX_FOCOS]; // armazeno todos os pontosX de foco
-		int pontosY[MAX_FOCOS]; // armazeno todos os pontosX de foco
-		double** meuGrafoMatriz; // matriz que armazena as distancias
-		
+		int vertices; 			  // numero de vértices
+		int pontosX[MAX_FOCOS];   // armazeno todos os pontosX de foco
+		int pontosY[MAX_FOCOS];   // armazeno todos os pontosX de foco
+		double** meuGrafoMatriz;  // matriz que armazena as distancias
+		bool** solucaoEncontrada; // marca quais soluções já foram encontradas
+		double** valorEncontrado; // salva os valores encontrados para cada solução
+	
 	public:
 		// Construtor da classe e suas atribuições
 		Grafo(int numVertices){
 			vertices = numVertices;
 			meuGrafoMatriz = new double*[numVertices];
+			solucaoEncontrada = new bool*[numVertices];
+			valorEncontrado = new double*[numVertices];
+			
+			// Para cada vertice, temos a possibilidade de ele já ter sido ou não visitado
+			// logo temos 2^numVertices possibilidades
+			int numPossibilidades = 1 << numVertices; // 1<<n é o mesmo que 2^n
+
 			for (int i = 0; i < numVertices; i++) {
 				meuGrafoMatriz[i] = new double[numVertices];
+				solucaoEncontrada[i] = new bool[numPossibilidades];
+				valorEncontrado[i] = new double[numPossibilidades];
 			}
 			
 			for (int i = 0; i < numVertices; i++) {
@@ -45,6 +56,10 @@ class Grafo{
 					if(i == j){
 						meuGrafoMatriz[i][j] = 0;
 					}
+				}
+				// A principio nenhuma solução foi encontrada
+				for (int j = 0; j < numPossibilidades; j++){
+					solucaoEncontrada[i][j] = false;
 				}
 				//cout << "todos inf" << endl;
 			}
@@ -56,7 +71,9 @@ class Grafo{
 		void calculaDistancia();
 		double distanciaEntreDoisPontos(int pX1, int pY1, int pX2, int pY2);
 		double travllingSalesmanProblem(int s);
-		
+		double encontraSolucao(int at, int vis, int nVis, int s);
+		bool visitado(int vertice, int verticesVisitados);
+		int visita(int vertice, int verticesVisitados);
 };
 
 // Função para debugar a matriz
@@ -93,38 +110,51 @@ double Grafo::distanciaEntreDoisPontos(int pX1, int pY1, int pX2, int pY2){
 	return dist;
 }
 
+// Checa se um vertice já foi visitado
+bool Grafo::visitado(int vertice, int verticesVisitados){
+	return ((1 << vertice) & verticesVisitados) > 0;
+}
+
+// Marca um vértice como visitado
+int Grafo::visita(int vertice, int verticesVisitados){
+	return (1 << vertice) | verticesVisitados;
+}
+
+double Grafo::encontraSolucao(int at, int vis, int nVis, int s){
+	// Se todos os vertices já foram visitados, retorna para casa
+	if (nVis == vertices){
+		return meuGrafoMatriz[at][s];
+	}
+	// Procura a solução para o caso atual somente se ela ainda não foi encontrada
+	if (!solucaoEncontrada[at][vis]){
+		double melhorSolucao = INT_MAX;
+		
+		for (int i = 0; i < vertices; i++){
+			// Verifica se o vertice 'i' ainda não foi visitado
+			if(!visitado(i, vis)){
+				// Uma possivel solução é visitar o vertice 'i' após o vertice 'at'
+				// Soma a distancia para ir do vertice 'at' ao vertice 'i'
+				double solucao = encontraSolucao(i, visita(i, vis), nVis+1, s) + meuGrafoMatriz[at][i];
+					
+				melhorSolucao = min(melhorSolucao, solucao);
+			}
+		}
+		// Marca que a solução para o caso atual ja foi encontrada
+		solucaoEncontrada[at][vis] = true;
+		// Salva o valor encontrado para evitar a necessidade de calculá-lo novamente
+		valorEncontrado[at][vis] = melhorSolucao;
+		
+	}
+	// retorna o valor encontrado para o caso atual
+	return valorEncontrado[at][vis];
+}
+
 // Problema resolvido com TSP
 double Grafo::travllingSalesmanProblem(int s){  
-    // Pego todos os vértices a partir do vértice passado por parâmetro
-    vector<int> verticesTSP; 
-    for (int i = 0; i < vertices; i++){
-        if (i != s) {
-            verticesTSP.push_back(i);
-		}
-	}
+	// Começa a procura da solução partindo do ponto inicial,
+	// somente o vertice s foi visitado até o momento
+	return encontraSolucao(s, visita(s, 0), 1, s);
 	
-    // Recupero a menor distância do Grafo Hamiltoniano(ciclo)
-    double min_path = INT_MAX; 
-    
-    // Função que reorganiza os elementos no intervalo [primeiro, último)
-    // na próxima permutação lexicograficamente maior.
-    while(next_permutation(verticesTSP.begin(), verticesTSP.end())){
-		// Inicializo a minha variável de caminho Atual como 0
-        double caminhoAtual = 0; 
-          
-        // Incremento as distâncias
-        int k = s; 
-        for (int i = 0; i < (int)verticesTSP.size(); i++) { 
-            caminhoAtual += meuGrafoMatriz[k][verticesTSP[i]]; 
-            k = verticesTSP[i]; 
-        } 
-        caminhoAtual += meuGrafoMatriz[k][s]; 
-  
-        // Atualizo o menor caminho
-        min_path = min(caminhoAtual, min_path); 
-	}
-	// Retorno o menor caminho
-    return min_path; 
 } 
 
 int main(){
